2605e064697a6adfc8a4a4ff4b3eab0068784e0b
Edward Thomson <ethomson@edwardthomson.com>
2019-03-24 20:49:57 -0700

    http: stop on server EOF
    We stop the read loop when we have read all the data.  We should also
    consider the server's feelings.
    
    If the server hangs up on us, we need to stop our read loop.  Otherwise,
    we'll try to read from the server - and fail - ad infinitum.

--- src/transports/http.c
+++ src/transports/http.c
@@ -932,8 +932,13 @@ replay:
 	while (!bytes_read && !t->parse_finished) {
 		t->parse_buffer.offset = 0;
 
-		if ((error = gitno_recv(&t->parse_buffer)) < 0)
+		if ((error = gitno_recv(&t->parse_buffer)) < 0) {
 			goto done;
+		} else if (error == 0) {
+			git_error_set(GIT_ERROR_NET, "unexpected disconnection from server");
+			error = -1;
+			goto done;
+		}
 
 		/*
 		 * This call to http_parser_execute will invoke the on_*
@@ -1176,8 +1181,13 @@ replay:
 
 		data_offset = t->parse_buffer.offset;
 
-		if ((error = gitno_recv(&t->parse_buffer)) < 0)
+		if ((error = gitno_recv(&t->parse_buffer)) < 0) {
 			goto done;
+		} else if (error == 0) {
+			git_error_set(GIT_ERROR_NET, "unexpected disconnection from server");
+			error = -1;
+			goto done;
+		}
 
 		/*
 		 * This call to http_parser_execute will result in invocations
